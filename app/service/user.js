3db692f529ae0a2a4ddd179214fecd8b132ea9a7
xiaqiubo <xiaqiubo123@163.com>
2018-01-03 09:21:51 +0800

    增加删除用户和更新用户权限函数

--- app/service/user.js
+++ app/service/user.js
@@ -1,16 +1,17 @@
 const db = require('./model')
+const libArray = require('../libs/array')
 const { user, usermeta, post, postmeta, comment, commentmeta, term } = db.models
 
-
-/*
- *   在用户注册注销时使用
- *   @parmas     {string}        用户名字符串
- *   @return     {Object}        用户相关信息(用户名/密码/邮箱/状态)
- * */
+const userSchema = ['id', 'username', 'password', 'email', 'status', 'authority', 'loginedAt', 'createdAt', 'updatedAt']
+    /*
+     *   在用户注册注销时使用
+     *   @parmas     {string}        用户名字符串
+     *   @return     {Object}        用户相关信息(用户名/密码/邮箱/状态)
+     * */
 user.findByName = async(name) => {
     // 如果用户名为空,则直接返回空
     if (!name) {
-        return null
+        return 10001
     }
     let ops = {
         where: {
@@ -28,8 +29,8 @@ user.findByName = async(name) => {
  * */
 user.findById = async(id) => {
     // 如果传入的id不是数字,直接返回空
-    if (!Number(id)) {
-        return null
+    if (!parseInt(id)) {
+        return 10006
     }
     let ops = {
         where: {
@@ -45,8 +46,8 @@ user.findById = async(id) => {
 
 user.findByIdAndPost = async(id) => {
     // 如果传入的id不是数字,直接返回空
-    if (!Number(id)) {
-        return null
+    if (!parseInt(id)) {
+        return 10006
     }
     let ops = {
         where: {
@@ -63,8 +64,8 @@ user.findByIdAndPost = async(id) => {
 
 user.findByIdAndComment = async(id) => {
     // 如果传入的id不是数字,直接返回空
-    if (!Number(id)) {
-        return null
+    if (!parseInt(id)) {
+        return 10006
     }
     let ops = {
         where: {
@@ -79,16 +80,18 @@ user.findByIdAndComment = async(id) => {
     return await user.findOne(ops)
 }
 
+
+// 增加用户(注册)
 user.signup = async(obj) => {
     // 如果用户名密码不存在,直接返回
     if (!obj.username || !obj.password) {
-        return null
+        return 10002
     }
 
     let user = await user.findByName(obj.username)
         // 如果用户已存在,直接返回空
     if (!user) {
-        return null
+        return 10003
     }
 
     let ops = {
@@ -99,6 +102,46 @@ user.signup = async(obj) => {
     return await user.create(ops)
 }
 
+// 更新user主表信息
+user.updateMainById = async(id, obj) => {
+    if (!parseInt(id)) {
+        return 10006
+    }
+    return await user.update(obj, { where: { id: parseInt(id) } })
+}
+
+// 更新主表及附加表,返回用户完整信息
+user.updateAndMetaById = async(id, obj) => {
+    if (!parseInt(id)) {
+        return 10006
+    }
+    await user.updateMainById(id, obj)
+    let keys = libArray(Object.keys(obj), userSchema)
+    keys.map(item => {
+        await usermeta.create({ ukey: item, uvalue: obj[item] })
+    })
+    return await user.findById(id, {
+        include: [
+            { model: usermeta }
+        ]
+    })
+}
+
+// 删除用户
+user.deleteById = async(id) => {
+    if (!parseInt(id)) {
+        return 10006
+    }
+    return await user.delete({ where: { id: id } })
+}
+
+// 修改用户权限
+user.authById = async(id, obj) => {
+    if (!parseInt(id)) {
+        return 10006
+    }
+    return user.updateMainById(id, obj)
+}
 
 
 module.exports = user
