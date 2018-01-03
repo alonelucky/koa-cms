85323570f200bce139eb5a2f3a963bd545c7f3e8
xiaqiubo <xiaqiubo123@163.com>
2018-01-03 16:12:51 +0800

    修复crypto调用错误

--- app/libs/hash.js
+++ app/libs/hash.js
@@ -1,18 +1,17 @@
-const crrpto = require('crypto')
+const crypto = require('crypto')
 
-module.exports= {
+module.exports = {
     hash256,
     csrf
 }
 
 // 常规混淆
-function hash256(string,key) {
-    return crrpto.createHamc('sha256',key).update(string).degist('hex')
+function hash256(string, key) {
+    return crypto.createHmac('sha256', key).update(string).digest('hex')
 }
 
 // csrf时间戳随机混淆
 function csrf(key) {
-    let random = Date.now()+''+Math.random()
-    hash256(random,key)
+    let random = Date.now() + '' + Math.random()
+    hash256(random, key)
 }
-
